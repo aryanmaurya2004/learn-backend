023c36f4079a47fc04ec4fe20e0985f0f6cbf0e2
aryan <mauryaaryan221306@gmail.com>
2026-03-24 11:32:15 +0530

    change route demo

--- index.mjs
+++ index.mjs
@@ -2,6 +2,14 @@ import express from 'express'
 const server = express()
 const port = 5000
 
+
+// server.get("/"),(req,res) =>{
+//     res.josn({massage:"apple"})
+// }
+server.get("/hello"),(req,res)=>{
+    res.josn({massage:"banana mane kela "})
+}
+
 server.listen(port,()=>{
     console.log(`server started on ${port}`)
 })
